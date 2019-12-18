9ec9204f659d35afc688133d2ade77cdb5e8cd50
youxuehu <youxuehu@youxuehudeiMac.local>
2019-12-18 22:42:58 +0800

    zk 节点创建

--- src/main/java/com/example/springbootdemo/common/zookeeper/ZkService.java
+++ src/main/java/com/example/springbootdemo/common/zookeeper/ZkService.java
@@ -1,6 +1,7 @@
 package com.example.springbootdemo.common.zookeeper;
 
 import org.I0Itec.zkclient.ZkClient;
+import org.I0Itec.zkclient.ZkLock;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
@@ -21,8 +22,30 @@ public class ZkService implements InitializingBean {
         }
     }
 
+    public String createEphemeral(String path){
+        ZkClient zkClient = new ZkClient(connection);
+        try {
+            zkClient.createEphemeral(path);
+            return "ok";
+        } catch (Exception e) {
+            return "fail";
+        }
+    }
+
+    public String lock(String path){
+        ZkClient zkClient = new ZkClient(connection);
+        try {
+            ZkLock eventLock = zkClient.getEventLock();
+            eventLock.lock();
+            return "ok";
+        } catch (Exception e) {
+            return "fail";
+        }
+    }
+
     @Override
     public void afterPropertiesSet() throws Exception {
         creteNode("/node2");
+        createEphemeral("/youxuehu");
     }
 }
