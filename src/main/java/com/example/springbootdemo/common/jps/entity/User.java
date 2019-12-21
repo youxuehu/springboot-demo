c7de82765ed8e909244214c5a72a2e6284b4e46f
youxuehu <youxuehu@youxuehudeiMac.local>
2019-12-21 20:10:47 +0800

    mysql connect

---
+++ src/main/java/com/example/springbootdemo/common/jps/entity/User.java
@@ -0,0 +1,46 @@
+package com.example.springbootdemo.common.jps.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@Setter
+public class User {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String userName;
+
+    @Column(nullable = false)
+    private String passWord;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = true, unique = true)
+    private String nickName;
+
+    @Column(nullable = false)
+    private String regTime;
+
+    public User(String userName, String passWord, String email, String nickName, String regTime) {
+        this.userName = userName;
+        this.passWord = passWord;
+        this.email = email;
+        this.nickName = nickName;
+        this.regTime = regTime;
+    }
+
+    public User() {
+    }
+
+}
