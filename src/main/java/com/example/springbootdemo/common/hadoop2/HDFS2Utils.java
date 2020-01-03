d06a4f3a41786d460a8aaa4e1aa4db248ef97ef4
youxuehu <youxuehu@youxuehudeiMac.local>
2020-01-03 22:41:04 +0800

    fix

--- src/main/java/com/example/springbootdemo/common/hadoop2/HDFS2Utils.java
+++ src/main/java/com/example/springbootdemo/common/hadoop2/HDFS2Utils.java
@@ -9,20 +9,21 @@ import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
+import java.io.InputStream;
 import java.util.List;
 
 @Component
 @Slf4j
-public class HDFSUtils implements ApplicationRunner {
+public class HDFS2Utils implements ApplicationRunner {
+
     @Value("${hdfs.master}")
-    String hdfsUrl;
+    private String hdfsMaster;
+
+    Configuration configuration = null;
 
     public void mkdir() throws Exception {
-        Configuration conf = new Configuration();
-        conf.set("fs.defaultFS", hdfsUrl);
-        conf.setBoolean("dfs.support.append", true);
-        System.setProperty("HADOOP_USER_NAME", "root");
-        FileSystem fs = FileSystem.get(conf);
+
+        FileSystem fs = FileSystem.get(configuration);
         if (!fs.exists(new Path("/tiger/node"))) {
             boolean mkdirs = fs.mkdirs(new Path("/tiger/node"));
         }
@@ -43,8 +44,9 @@ public class HDFSUtils implements ApplicationRunner {
         }
     }
 
-    public void readFile(FileSystem fs) throws Exception {
-        FSDataInputStream inputStream = fs.open(new Path("/tiger/node/1.txt"));
+    public void readFile(String path) throws Exception {
+        FileSystem fs = FileSystem.get(configuration);
+        FSDataInputStream inputStream = fs.open(new Path(path));
         List<String> readLines = IOUtils.readLines(inputStream, "UTF-8");
         for (String line : readLines) {
             log.info("{}", line);
@@ -52,13 +54,22 @@ public class HDFSUtils implements ApplicationRunner {
         log.info("{}", readLines.size());
     }
 
-    public void uploadFIle(FileSystem fs) throws Exception {
-        fs.copyFromLocalFile(new Path("/Users/youxuehu/IdeaProjects/springboot-demo/src/main/java/com/example/springbootdemo/common/hadoop2/HDFSUtils.java"), new Path("/tiger/node"));
+    public void uploadFIle(String localPath, String hdfsPath) throws Exception {
+        FileSystem fs = FileSystem.get(configuration);
+        fs.copyFromLocalFile(new Path(localPath), new Path(hdfsPath));
         log.info("{}", "upload file success");
     }
 
-    public void fileList(FileSystem fs) throws Exception {
-        RemoteIterator<LocatedFileStatus> listFiles = fs.listFiles(new Path("/"), true);
+    public void uploadFIle(InputStream inputStream, String fileName) throws Exception {
+        FileSystem fs = FileSystem.get(configuration);
+        FSDataOutputStream outputStream = fs.create(new Path("/" + fileName));
+        IOUtils.copyLarge(inputStream, outputStream, new byte[2048]);
+        log.info("{}", "upload file success");
+    }
+
+    public void fileList(String path) throws Exception {
+        FileSystem fs = FileSystem.get(configuration);
+        RemoteIterator<LocatedFileStatus> listFiles = fs.listFiles(new Path(path), true);
         while (listFiles.hasNext()) {
             LocatedFileStatus fileStatus = listFiles.next();
             log.info("{}", fileStatus);
@@ -68,13 +79,17 @@ public class HDFSUtils implements ApplicationRunner {
     @Override
     public void run(ApplicationArguments args) throws Exception {
 //        mkdir();
-        Configuration conf = new Configuration();
-        conf.set("fs.defaultFS", hdfsUrl);
-        conf.setBoolean("dfs.support.append", true);
+//        Configuration conf = new Configuration();
+//        conf.set("fs.defaultFS", hdfsUrl);
+//        conf.setBoolean("dfs.support.append", true);
+//        System.setProperty("HADOOP_USER_NAME", "root");
+//        FileSystem fs = FileSystem.get(conf);
+//        readFile(fs);
+//        uploadFIle(fs);
+//        fileList(fs);
+        configuration = new Configuration();
+        configuration.set("fs.defaultFS", hdfsMaster);
+        configuration.setBoolean("dfs.support.append", true);
         System.setProperty("HADOOP_USER_NAME", "root");
-        FileSystem fs = FileSystem.get(conf);
-        readFile(fs);
-        uploadFIle(fs);
-        fileList(fs);
     }
 }
