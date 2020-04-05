6ffb26ae3e3a7b95e2fd1167c5a8fb20d110a60d
吕水菡 <lvshuihan@lvshuihandeMacBook-Pro.local>
2020-04-05 19:46:32 +0800

    add redis

---
+++ src/main/java/com/example/springbootdemo/common/redis/RedisComonent.java
@@ -0,0 +1,24 @@
+package com.example.springbootdemo.common.redis;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+public class RedisComonent implements InitializingBean {
+
+    Logger LOG = LoggerFactory.getLogger(RedisComonent.class);
+
+    @Autowired
+    RedisTemplate redisTemplate;
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        redisTemplate.opsForValue().set("name_redis", "recommend system");
+        String name = (String) redisTemplate.opsForValue().get("name_redis");
+        LOG.info("name=>{}", name);
+    }
+}
