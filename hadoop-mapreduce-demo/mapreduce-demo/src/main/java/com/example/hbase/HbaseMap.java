bcc84f1b1b47c41b1c559233f119c9d607256f3c
youxuehu <youxuehu@hotmail.com>
2020-06-14 22:04:19 +0800

    mapreduce hbase

---
+++ hadoop-mapreduce-demo/mapreduce-demo/src/main/java/com/example/hbase/HbaseMap.java
@@ -0,0 +1,37 @@
+package com.example.hbase;
+
+import org.apache.hadoop.hbase.Cell;
+import org.apache.hadoop.hbase.CellUtil;
+import org.apache.hadoop.hbase.client.Result;
+import org.apache.hadoop.hbase.io.ImmutableBytesWritable;
+import org.apache.hadoop.hbase.mapreduce.TableMapper;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+
+import java.io.IOException;
+import java.util.List;
+
+/**
+ *  * 继承的是hbase中提供的TableMapper类，其实这个类也是继承的MapReduce类。 后边跟的两个泛型参数指定类型是mapper输
+ *  * 出的数据类型，该类型必须继承自Writable类， 例如可能用到的put和delete就可以。 需要注意的是要和initTableMapperJob
+ *  * 方法指定的数据类型一直。 该过程会自动从指定hbase表内一行一行读取数据进行处理。
+ *  *
+ */
+public class HbaseMap  extends TableMapper<Text, IntWritable> {
+    //  key：  代表hbase中的每一行的key  value：代表此key对于的值（column）
+    @Override
+    protected void map(ImmutableBytesWritable key, Result value,
+                       Context context)
+            throws IOException, InterruptedException {
+        System.out.println(new String(key.get())+"---->"+value.size());
+        List<Cell> cells = value.listCells();
+        for (Cell cell : cells) {
+            System.out.print("RowName:"+new String(CellUtil.cloneRow(cell)));
+            System.out.print("--->Timetamp:"+cell.getTimestamp());
+            System.out.print("--->column Family:"+new String(CellUtil.cloneFamily(cell)));
+            System.out.print("---->cell Name:"+new String(CellUtil.cloneQualifier(cell)));
+            System.out.println("---->cell value:"+new String(CellUtil.cloneValue(cell)));
+        }
+        context.write(new Text("aa"),new IntWritable(1));
+    }
+}
---
+++ hadoop-mapreduce-demo/mapreduce-demo/src/main/java/com/example/hbase/HbaseRed.java
@@ -0,0 +1,34 @@
+package com.example.hbase;
+
+import org.apache.hadoop.hbase.client.Put;
+import org.apache.hadoop.hbase.mapreduce.TableReducer;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.io.Text;
+/**
+ * reducer继承的是TableReducer类。 后边指定三个泛型参数，前两个必须对应map过程的输出key/value类型， 第三个必须是
+ * put或者delete。write的时候可以把key写null，它是不必要的。 这样reducer输出的数据会自动插入outputTable指定的 表内。
+ *
+ * @author Administrator
+ *
+ */
+import java.io.IOException;
+import java.util.Iterator;
+
+public class HbaseRed  extends TableReducer<Text, IntWritable, NullWritable> {
+    public void reduce(Text key, Iterable<IntWritable> values, Context context)
+            throws IOException, InterruptedException {
+        System.out.println("in method reduce..."+key.toString());
+        Iterator<IntWritable> iterator = values.iterator();
+        System.out.println(key.toString());
+        while (iterator.hasNext()) {
+            int age = iterator.next().get();
+            System.out.println(age);
+        }
+
+        // reducer逻辑
+        Put put = new Put("aa".getBytes());
+        put.addColumn("info".getBytes(), "age".getBytes(), "12".getBytes());
+        context.write(NullWritable.get(), put);
+    }
+}
