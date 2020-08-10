86651b2f0b07966f7ade0abea293fc450003f34f
youxuehu <youxuehu@hotmail.com>
2020-08-10 22:42:58 +0800

    Flink 大数据论文

--- src/main/scala/com/alipay/common/WordCount2.scala
+++ src/main/scala/com/alipay/common/WordCount2.scala
@@ -10,40 +10,35 @@ import org.apache.spark.{SparkConf, SparkContext}
 /**
  * 统计 word count
  */
-object WordCount {
+object WordCount2 {
   def main(args: Array[String]): Unit = {
     val conf = new SparkConf()
     conf.setMaster("local")
     conf.setAppName("wordcount")
     val sc = new SparkContext(conf)
-    val rdd = sc.textFile("hdfs://localhost:9000/The_Man_of_Property.txt")
+    val rdd = sc.textFile("hdfs://localhost:9000/Spark/Input/WordCount/*")
     val newRdd = rdd.repartition(1)
     // 计算文章中单词出现的个数，并按照个数倒叙排序, 截取前10条数据
     val data = newRdd.flatMap(_.split(" "))
       .map((_,1)) // map  输出
       .reduceByKey(_+_) // reduce计算
       .sortBy(_._2,false) // sort排序
-      .take(10) // 取前10行
+      .repartition(1)
       .map{x =>
             x._1 + "\t" + x._2
-          }
-    /**
-     * hadoop
-     */
+      }
     val hadoopConf = sc.hadoopConfiguration
     val hdfs = org.apache.hadoop.fs.FileSystem.get(hadoopConf)
-    val filePath = "hdfs://localhost:9000/tmp/wordcount"
+    val filePath = "hdfs://localhost:9000/Spark/Output/WordCount"
     val path = new Path(filePath);
     if(hdfs.exists(path)){
-      /**
-       * 如果输出目录存在，则删除
-       */
+      // 如果输出目录存在，则删除
       hdfs.delete(path)
     }
-    data.foreach(println)
+    // data.foreach(println)
     // 将list转换成rdd
-    val new_data = sc.parallelize(data)
-    // 写入HDFS
+    val new_data = sc.parallelize(data.collect())
+    // 最终结果写入HDFS
     new_data.saveAsTextFile(filePath)
   }
 }
