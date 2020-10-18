ed9f14bc2962e3c04273b0dd2cb7baa48e81dfec
youxuehu <youxuehu@hotmail.com>
2020-10-18 22:36:48 +0800

    copy file

---
+++ src/main/java/com/example/springbootdemo/controller/ThreadPoolController.java
@@ -0,0 +1,19 @@
+package com.example.springbootdemo.controller;
+
+import com.example.springbootdemo.utils.file.copy.ThreadPoolMain;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/thread/pool")
+public class ThreadPoolController {
+
+    @Autowired
+    private ThreadPoolMain threadPoolMain;
+
+    @RequestMapping("copyFile")
+    public void copyFile() {
+        threadPoolMain.copyFile("", "", 5);
+    }
+}
