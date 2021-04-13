deda0d807b4a740e7c561e67f1d8f87208b0be58
youxuehu <youxuehu@hotmail.com>
2021-04-13 22:08:19 +0800

    原型模式

---
+++ src/main/java/com/example/springbootdemo/common/设计模式/原型模式/demo2/Student.java
@@ -0,0 +1,20 @@
+package com.example.springbootdemo.common.设计模式.原型模式.demo2;
+
+/**
+ * @author youxuehu
+ * @version v1.0
+ * @date 2021/4/13 10:00 下午
+ * @desrription 这是类的描述信息
+ */
+public class Student {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
