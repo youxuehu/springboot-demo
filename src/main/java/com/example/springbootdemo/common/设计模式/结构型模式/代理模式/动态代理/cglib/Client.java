e18134aa1cc558d8f94abd79976231e40510928b
youxuehu <youxuehu@hotmail.com>
2021-04-17 11:47:40 +0800

    cglib动态代理，给没有实现接口的类生成代理对象

---
+++ src/main/java/com/example/springbootdemo/common/设计模式/结构型模式/代理模式/动态代理/cglib/Client.java
@@ -0,0 +1,16 @@
+package com.example.springbootdemo.common.设计模式.结构型模式.代理模式.动态代理.cglib;
+
+
+/**
+ * @author youxuehu
+ * @version v1.0
+ * @date 2021/4/17 11:37 上午
+ * @desrription 这是类的描述信息
+ */
+public class Client {
+    public static void main(String[] args) {
+        CglibProxyFactory proxyFactory = new CglibProxyFactory();
+        TranAction proxyObject = proxyFactory.proxyObject();
+        proxyObject.sell();
+    }
+}
